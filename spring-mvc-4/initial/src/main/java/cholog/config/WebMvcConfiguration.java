6ded593c5c65e23eec7e67ce67b3f58ea673f98b
Seokmyung Ham <gkatjraud1@gmail.com>
2024-05-07 14:18:56 +0900

    feat: WebMvcConfigurationTest가 성공하도록 config 기능 구현

--- spring-mvc-4/initial/src/main/java/cholog/config/WebMvcConfiguration.java
+++ spring-mvc-4/initial/src/main/java/cholog/config/WebMvcConfiguration.java
@@ -1,5 +1,7 @@
 package cholog.config;
 
+import cholog.ui.AuthenticationPrincipalArgumentResolver;
+import cholog.ui.CheckLoginInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -13,15 +15,19 @@ public class WebMvcConfiguration implements WebMvcConfigurer {
     // TODO: "/" 요청 시 hello.html 페이지 응답하기
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("hello");
     }
 
     // TODO: "/admin/**" 요청 시 LoginInterceptor 동작하게 하기
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CheckLoginInterceptor())
+                .addPathPatterns("/admin/**");
     }
 
     // TODO: AuthenticationPrincipalArgumentResolver 등록하기
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(new AuthenticationPrincipalArgumentResolver());
     }
 }
